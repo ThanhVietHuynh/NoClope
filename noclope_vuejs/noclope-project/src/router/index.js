70eb35a9fb3e097385f18c5458da559bd009f0c9
ThanhVietHuynh <huynhthanhviet13006@gmail.com>
2022-12-08 14:47:57 +0100

    Home page et correction

--- noclope_vuejs/noclope-project/src/router/index.js
+++ noclope_vuejs/noclope-project/src/router/index.js
@@ -4,6 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path:"/",
+      name:"home",
+      component: () => import("../views/HomeView.vue")
+    },
+    {
       path: "/register",
       name: "register",
       component: () => import("../views/RegisterView.vue")
@@ -11,59 +16,19 @@ const router = createRouter({
     {
       path: "/contact",
       name: "contact",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/ContactsView.vue"),
     },
     {
       path: "/project",
       name: "project",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/ProjectView.vue"),
     },
     {
       path: "/login",
       name: "login",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/LoginView.vue"),
     },
-    // {
-    //   path: "/profil",
-    //   name: "profil",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/ProfilView.vue"),
-    // },
-    // {
-    //   path: "/login",
-    //   name: "login",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/LoginView.vue"),
-    // },
-    // {
-    //   path: "/edit",
-    //   name: "edit",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/EditView.vue"),
-    // },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
+
   ],
 });
 
