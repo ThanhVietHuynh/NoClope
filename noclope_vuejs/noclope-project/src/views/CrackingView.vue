ef748de7961b9e45299c99319f6249f73231744c
nabil <nabil-13130@hotmail.fr>
2022-12-08 10:14:29 +0100

    Les vues

---
+++ noclope_vuejs/noclope-project/src/views/CrackingView.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <form action="" method="post">
+        <p>
+            <label for="smoked">Nombre de cigarettes fumées : 
+                <input type="number" name="smoked" id="smoked">
+            </label>
+        </p>
+        <p>
+            <label for="crackDate">J'ai craqué quand ? 
+                <input type="date" name="crackDate" id="crackDate">
+            </label>
+        </p>
+    </form>
+</template>
---
+++ noclope_vuejs/noclope-project/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Se connecter</h1>
+    <form action="" method="post">
+        <p>
+            <label for="email">Email : 
+                <input type="text" name="email" id="email">
+            </label>
+        </p>
+        <p>
+            <label for="password">Mot de passe : 
+                <input type="password" name="password" id="password">
+            </label>
+        </p>
+    </form>
+</template>
