f8455473d70f0b825cd0f8890312c6427c2d70c4
Marlee Alcaraz <marlee.alcaraz@gmail.com>
2022-12-09 17:18:05 +0100

    NavBar

---
+++ noclope_vuejs/noclope-project/src/components/NavBar.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+    <div class="bg-gray-900 text-gray-100 py-3 px-6 shadow md:flex justify-between items-center">
+    <div class="flex items-center">
+        <img src="@/assets/logoNoclope.png" class="logoNoclope" alt="">
+    </div>
+     <nav> 
+         <ul class="md-flex md:items-center md:px-0 px-3 md:pb-0 pb-10 md:static absolute bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in flex flex">
+            <li class="md:mx-4 md:my-0 my-6">
+                <RouterLink to="/" class="text-xl hover:text-blue-300">Accueil</RouterLink>
+            </li>
+            <li class="md:mx-4 md:my-0 my-6">
+                <RouterLink to="/project" class=" text-xl hover:text-blue-300">Mon Projet</RouterLink>
+            </li>
+            <li class="md:mx-4 md:my-0 my-6">
+                <RouterLink to="/contact" class=" text-xl hover:text-blue-300">Contact</RouterLink>
+            </li>
+            <li class="md:mx-4 md:my-0 my-6">
+                <RouterLink to="/register" class=" text-xl hover:text-blue-300">S'enregistrer</RouterLink>
+            </li>
+            <li class="md:mx-4 md:my-0 my-6">
+                <RouterLink to="/login" class=" text-xl hover:text-blue-300">Se connecter</RouterLink>
+            </li>
+            <li class="md:mx-4 md:my-0 my-6">
+                <RouterLink to="/cracking" class=" text-xl hover:text-blue-300">J'ai craqué</RouterLink>
+            </li>
+        </ul>
+     </nav>
+           
+
+       <!-- <ul class="md-flex md:items-center md:px-0 px-3 md:pb-0 pb-10 md:static absolute bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in">
+        <li class="md:mx-4 md:my-0 my-6"> 
+            <a href="RouterLink in RouterLinks" class="text-xl hover:text-green-500">{{link.name}}</a>
+        </li>
+      </ul>  -->
+
+
+   
+    </div>
+</template>
+
+<script>
+export default{
+setup(){
+    // let Links=[
+    //     {name : "Accueil", link:"/"},
+    //     {name : "Mon objectif", link:"/project"},
+    //     {name : "Mes contacts", link:"/contact"},
+    //     {name : "J'ai craqué", link:"/cracking"},
+    //     {name : "S'inscrire", link:"/login"},
+    //     {name : "Se connecter", link:"/register"},
+    // ]
+}
+}
+</script>
+
+<style scoped>
+
+.logoNoclope{
+    height: 40px;
+    width: 145px;
+}
+</style>
