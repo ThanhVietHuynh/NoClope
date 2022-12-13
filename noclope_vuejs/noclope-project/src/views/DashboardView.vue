b20ff0e3f04120eaeeb47e1287a226c120bf2995
Mathilde Gamore <malega89@hotmail.com>
2022-12-13 14:45:19 +0100

    dash

---
+++ noclope_vuejs/noclope-project/src/views/DashboardView.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+import { def } from '@vue/shared';
+import Register from '../components/Register.vue';
+</script>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+        async getProjects() {
+        const response = await fetch("http://127.0.0.1:8000/api/project", {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        });
+
+        const data = await response.json();
+
+        this.projects = data.projects;
+      },
+
+        mounted() {
+
+        },
+    }
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
