5bf20e2d48d8eb9301ca483e647cbde461d6bf3a
Marlee Alcaraz <marlee.alcaraz@gmail.com>
2022-12-19 16:14:15 +0100

    css

--- noclope_vuejs/noclope-project/src/components/EditProfil.vue
+++ noclope_vuejs/noclope-project/src/components/EditProfil.vue
@@ -160,7 +160,7 @@ export default{
       w-full
       px-6
       py-2.5
-      bg-blue-600
+      bg-teal-500
       text-white
       font-medium
       text-xs
@@ -168,9 +168,9 @@ export default{
       uppercase
       rounded
       shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
+      hover:bg-teal-400 hover:shadow-lg
+      focus:bg-teal-400 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-teal-600 active:shadow-lg
       transition
       duration-150
       ease-in-out">Valider</button>
